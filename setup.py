d9e3413391591be9f2dd440af365023c43f2ada1
suchyta1 <eric.d.suchyta@gmail.com>
2015-09-02 13:42:56 -0400

    first commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name="suchyta_utils", 
+      version="0.1.0",
+      description="Utilities written by Eric Suchyta",
+      license = "GPL",
+      author="Eric Suchyta",
+      author_email="eric.d.suchyta@gmail.com",
+      packages=['suchyta_utils'])
