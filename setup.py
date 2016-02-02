0bc92f0480fa4aa3340ed891e7d35ff64b6a3627
Eric Suchyta <eric.d.suchyta@gmail.com>
2016-02-02 11:27:18 -0500

    added screen stuff

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 #from distutils.core import setup
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(name="suchyta_utils", 
       version="0.1.0",
       description="Utilities written by Eric Suchyta",
       license = "GPL",
-      author="Eric Suchyta",
-      author_email="eric.d.suchyta@gmail.com",
       packages=['suchyta_utils'],
       package_data={'suchyta_utils':['custom-sytle.mpl']},
-      include_package_data=True)
+      scripts=['bin/.screenrc-nest_inner','bin/.screenrc-nest_outer','bin/screen-split','bin/screen-retach','bin/screen-detach'],
+      author="Eric Suchyta",
+      author_email="eric.d.suchyta@gmail.com")
