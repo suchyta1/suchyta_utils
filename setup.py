d30756bb092f400014f26956229832de1aaf4ca7
Eric Suchyta <eric.d.suchyta@gmail.com>
2016-02-03 07:34:03 -0500

    added the script to concatenate pdfs to bin

--- setup.py
+++ setup.py
@@ -7,6 +7,6 @@ setup(name="suchyta_utils",
       license = "GPL",
       packages=['suchyta_utils'],
       package_data={'suchyta_utils':['custom-sytle.mpl']},
-      scripts=['bin/.screenrc-nest_inner','bin/.screenrc-nest_outer','bin/screen-split','bin/screen-retach','bin/screen-detach'],
+      scripts=['bin/.screenrc-nest_inner','bin/.screenrc-nest_outer','bin/screen-split','bin/screen-retach','bin/screen-detach','bin/catpdfs'],
       author="Eric Suchyta",
       author_email="eric.d.suchyta@gmail.com")
